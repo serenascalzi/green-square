1d89ec469b75ddd41dd00409d7a829eb16bd5f56
Serena Scalzi <serenascalzi@alumni.iu.edu>
2019-04-11 18:33:06 -0700

    added april 11 quote

--- main.js
+++ main.js
@@ -10,8 +10,9 @@ $(document).ready(function() {
 		'&ldquo;We are going to have peace even if we have to fight for it.&rdquo; <br /> &mdash;Dwight D. Eisenhower',
 		'&ldquo;Find something you&rsquo;re passionate about and keep tremendously interested in it.&rdquo; <br /> &mdash;Julia Child',
 		'&ldquo;Cherish all your happy moments; they make a fine cushion for old age.&rdquo; <br /> &mdash;Booth Tarkington',
+		'&ldquo;All change is not growth, as all movement is not forward.&rdquo; <br /> &mdash;Ellen Glasgow',
 		//'&ldquo; &rdquo; <br /> &mdash; ',
-		'&ldquo;April 11 Quote&rdquo; <br /> &mdash;Attribution'
+		'&ldquo;April 12 Quote&rdquo; <br /> &mdash;Attribution'
     ]
 
 	let marchArray = [
