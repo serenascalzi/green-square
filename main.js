ba606a10a31b7e83ab0d44e488f4409763a62bce
Serena Scalzi <serenascalzi@alumni.iu.edu>
2019-04-25 18:04:22 -0700

    added april 21 quote

--- main.js
+++ main.js
@@ -20,8 +20,9 @@ $(document).ready(function() {
 		'&ldquo;Better remain silent, better not even think, if you are not prepared to act.&rdquo; <br /> &mdash;Annie Besant',
 		'&ldquo;No matter what people tell you, words and ideas can change the world.&rdquo; <br /> &mdash;Robin Williams',
 		'&ldquo;God loves each of us as if there were only one of us.&rdquo; <br /> &mdash;Saint Augustine',
+		'&ldquo;A rebirth out of spiritual adversity causes us to become new creatures.&rdquo; <br /> &mdash;James E. Faust',
 		//'&ldquo; &rdquo; <br /> &mdash; ',
-		'&ldquo;April 21 Quote&rdquo; <br /> &mdash;Attribution'
+		'&ldquo;April 22 Quote&rdquo; <br /> &mdash;Attribution'
     ]
 
 	let marchArray = [
