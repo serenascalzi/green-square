e8fa6462b862645d945206bb58d5d7e852ba0e52
Serena Scalzi <serenascalzi@alumni.iu.edu>
2019-05-28 16:47:44 -0700

    added may 15 quote

--- main.js
+++ main.js
@@ -14,8 +14,9 @@ $(document).ready(function() {
 		'&ldquo;Mother is the name for God in the lips and hearts of little children.&rdquo; <br /> &mdash;William Makepeace Thackeray',
 		'&ldquo;The sun, the earth, love, friends, our very breath are parts of the banquet.&rdquo; <br /> &mdash;Rebecca Harding Davis',
 		'&ldquo;If you do not think about your future, you cannot have one.&rdquo; <br /> &mdash;John Galsworthy',
+		'&ldquo;Have patience. All things are difficult before they become easy.&rdquo; <br /> &mdash;Saadi',
 		//'&ldquo; &rdquo; <br /> &mdash; ',
-		'&ldquo;May 15 Quote&rdquo; <br /> &mdash;Attribution'
+		'&ldquo;May 16 Quote&rdquo; <br /> &mdash;Attribution'
     ]
 
 	let aprilArray = [
