6a498852b5daf943957d25d536c8a545f8b100dd
Serena Scalzi <serenascalzi@alumni.iu.edu>
2019-06-12 21:54:18 -0700

    added june 8 quote

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ $(document).ready(function() {
 		'&ldquo;Principles have no real force except when one is well-fed.&rdquo; <br /> &mdash;Mark Twain',
 		'&ldquo;It is the soothing thing about history that it does repeat itself.&rdquo; <br /> &mdash;Gertrude Stein',
 		'&ldquo;There are no shortcuts in evolution.&rdquo; <br /> &mdash;Louis D. Brandeis',
+		'&ldquo;Immorality: the morality of those who are having a better time.&rdquo; <br /> &mdash;H. L. Mencken',
 		//'&ldquo; &rdquo; <br /> &mdash; ',
-		'&ldquo;June 8 Quote&rdquo; <br /> &mdash;Attribution'
+		'&ldquo;June 9 Quote&rdquo; <br /> &mdash;Attribution'
 	]
 
 	let mayArray = [
