8d92ccb0f513b66dd7eeb5b30025dadf14b620c9
Serena Scalzi <serenascalzi@alumni.iu.edu>
2019-06-12 21:59:15 -0700

    added june 12 quote

--- main.js
+++ main.js
@@ -11,8 +11,9 @@ $(document).ready(function() {
 		'&ldquo;If you obey all the rules you miss all the fun.&rdquo; <br /> &mdash;Katharine Hepburn',
 		'&ldquo;No amount of experimentation can ever prove me right; a single experiment can prove me wrong.&rdquo; <br /> &mdash;Albert Einstein',
 		'&ldquo;You cannot shake hands with a clenched fist.&rdquo; <br /> &mdash;Indira Gandhi',
+		'&ldquo;Parents were invented to make children happy by giving them something to ignore.&rdquo; <br /> &mdash;Ogden Nash',
 		//'&ldquo; &rdquo; <br /> &mdash; ',
-		'&ldquo;June 12 Quote&rdquo; <br /> &mdash;Attribution'
+		'&ldquo;June 13 Quote&rdquo; <br /> &mdash;Attribution'
 	]
 
 	let mayArray = [
