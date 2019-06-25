7c0d97679c7eda58b2090f3b0974fa46d510c95b
Serena Scalzi <serenascalzi@alumni.iu.edu>
2019-06-25 16:16:01 -0700

    added june 20 quote

--- main.js
+++ main.js
@@ -19,8 +19,9 @@ $(document).ready(function() {
 		'&ldquo;It doesn&rsquo;t matter who my father was; it matters who I remember he was.&rdquo; <br /> &mdash;Anne Sexton',
 		'&ldquo;We cannot live only for ourselves. A thousand fibers connect us with our fellow men.&rdquo; <br /> &mdash;Herman Melville',
 		'&ldquo;Many a trip continues long after movement in time and space have ceased.&rdquo; <br /> &mdash;John Steinbeck',
+		'&ldquo;He who knows that enough is enough will always have enough.&rdquo; <br /> &mdash;Lao Tzu',
 		//'&ldquo; &rdquo; <br /> &mdash; ',
-		'&ldquo;June 20 Quote&rdquo; <br /> &mdash;Attribution'
+		'&ldquo;June 21 Quote&rdquo; <br /> &mdash;Attribution'
 	]
 
 	let mayArray = [
