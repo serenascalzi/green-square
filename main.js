bd50935600d7522468a20653a43d9156ca12febd
Serena Scalzi <serenascalzi@alumni.iu.edu>
2019-08-16 21:25:46 -0700

    added august 11 quote

--- main.js
+++ main.js
@@ -10,8 +10,9 @@ $(document).ready(function() {
 		'&ldquo;When one must, one can.&rdquo; <br /> &mdash;Charlotte Whitton',
 		'&ldquo;If we have no peace, it is because we have forgotten that we belong to each other.&rdquo; <br /> &mdash;Mother Teresa',
 		'&ldquo;Unless someone like you cares a whole awful lot, nothing is going to get better. It&rsquo;s not.&rdquo; <br /> &mdash;Dr. Seuss',
+		'&ldquo;Act as if what you do makes a difference. It does.&rdquo; <br /> &mdash;William James',
 		//'&ldquo; &rdquo; <br /> &mdash; ',
-		'&ldquo;August 11 Quote&rdquo; <br /> &mdash;Attribution'
+		'&ldquo;August 12 Quote&rdquo; <br /> &mdash;Attribution'
 	]
 
 	let julyArray = [
