fb8b17a81792cfe2b59a81bad56eabe599b8c2c4
Serena Scalzi <serenascalzi@alumni.iu.edu>
2019-09-23 19:16:15 -0700

    added september 23 quote

--- main.js
+++ main.js
@@ -22,8 +22,9 @@ $(document).ready(function() {
 		'&ldquo;Let us always meet each other with smile, for the smile is the beginning of love.&rdquo; <br /> &mdash;Mother Teresa',
 		'&ldquo;Give me the luxuries of life and I will willingly do without the necessities.&rdquo; <br /> &mdash;Frank Lloyd Wright',
 		'&ldquo;With the new day comes new strength and new thoughts.&rdquo; <br /> &mdash;Eleanor Roosevelt',
+		'&ldquo;I often quote myself. It adds spice to my conversation.&rdquo; <br /> &mdash;George Bernard Shaw',
 		//'&ldquo; &rdquo; <br /> &mdash; ',
-		'&ldquo;September 23 Quote&rdquo; <br /> &mdash;Attribution'
+		'&ldquo;September 24 Quote&rdquo; <br /> &mdash;Attribution'
 	]
 
 	let augustArray = [
