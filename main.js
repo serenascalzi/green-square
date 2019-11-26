9777d855f8faabba25586372cf957871a4c30e93
serenascalzi <serenascalzi@alumni.iu.edu>
2019-11-25 19:56:36 -0800

    added november 20 quote

--- main.js
+++ main.js
@@ -19,8 +19,9 @@ $(document).ready(function() {
 		'&ldquo;The tigers of wrath are wiser than the horses of instruction.&rdquo; <br /> &mdash;William Blake',
 		'&ldquo;If I can stop one heart from breaking, I shall not live in vain.&rdquo; <br /> &mdash;Emily Dickinson',
 		'&ldquo;Wise sayings often fall on barren ground, but a kind word is never thrown away.&rdquo; <br /> &mdash;Arthur Helps',
+		'&ldquo;We choose our joys and sorrows long before we experience them.&rdquo; <br /> &mdash;Khalil Gibran',
 		//'&ldquo; &rdquo; <br /> &mdash; ',
-		'&ldquo;November 20 Quote&rdquo; <br /> &mdash;Attribution'
+		'&ldquo;November 21 Quote&rdquo; <br /> &mdash;Attribution'
 	]
 
 	let octoberArray = [
