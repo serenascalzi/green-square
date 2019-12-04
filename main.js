0278b0eb1afd28e9d5d8b4c3dfb7ad0d89674525
serenascalzi <serenascalzi@alumni.iu.edu>
2019-12-03 19:32:26 -0800

    added december 1 quote

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	let decemberArray = [
+		'&ldquo;Soul meets soul on lovers&rsquo; lips.&rdquo; <br /> &mdash;Percy Bysshe Shelley',
 		//'&ldquo; &rdquo; <br /> &mdash; ',
-		'&ldquo;December 1 Quote&rdquo; <br /> &mdash;Attribution'
+		'&ldquo;December 2 Quote&rdquo; <br /> &mdash;Attribution'
 	]
 
 	let novemberArray = [
